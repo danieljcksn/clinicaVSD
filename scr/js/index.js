fc1522869a71b9ca9b85c5791a661158a0f7406f
daniielcavallcante <daniiel.cavallcante@gmail.com>
2020-02-26 02:50:08 -0300

    Carousel, Redes Sociais e Footer

--- scr/js/index.js
+++ scr/js/index.js
@@ -1,7 +1,13 @@
 $('.carousel.carousel-slider').carousel({
     fullWidth: true,
-    indicators: true
+    indicators: true, 
+    duration: 400,
 });
+autoplay();
+function autoplay() {
+    $('.carousel').carousel('next');
+    setTimeout(autoplay, 5000);
+}
 //--------------------------- Scroll Reveal ---------------------------//
 //Carousel
 ScrollReveal().reveal('#carousel', { delay: 500 });
@@ -36,12 +42,13 @@ ScrollReveal().reveal('#form', { duration: 2000 });
 //Contato
 ScrollReveal().reveal('#contato', { delay:500 });
 ScrollReveal().reveal('#contato', { duration: 2000 });
+
+//Social
+ScrollReveal().reveal('#titulo-social', { delay:500 });
+ScrollReveal().reveal('#titulo-social', { duration: 2000 });
+ScrollReveal().reveal('social-content', { delay:700 });
+ScrollReveal().reveal('social-content', { duration: 2000 });
 //---------------------------------------------------------------------//
-
-//Sidenav
-const slide_menu = document.querySelectorAll(".sidenav");
-M.Sidenav.init(slide_menu, {});
-
 // Inicialização do ScrollsPy
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.scrollspy');
@@ -49,42 +56,6 @@ document.addEventListener('DOMContentLoaded', function() {
     throttle: 50;
 });
 
-
-//Contador de Caracteres
-$(document).ready(function() {
-    $('input#input_text, textarea#textarea2').characterCounter();
-});
-
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {
-        slideIndex = 1
-    }
-    if (n < 1) {
-        slideIndex = slides.length
-    }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-}
+//Sidenav
+const slide_menu = document.querySelectorAll(".sidenav");
+M.Sidenav.init(slide_menu, {});
