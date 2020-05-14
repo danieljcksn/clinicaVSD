262e3e691008a46a964964e116370e6c24194b22
Nicholas Rodrigues dos Santos <nicholasybiologia@gmail.com>
2020-05-14 16:19:50 -0300

    second validation methods
    criada função de validação para validar e tratar os dados enviados ao servidor

--- scr/js/valid.js
+++ scr/js/valid.js
@@ -1,13 +1,29 @@
 function validateForm() {
-    const name = document.forms["myForm"]["fname"].value;
-    // const email = null
-    // const telefone = null
-    // const mensage = null
-    if (x == "") {
-        alert("Name must be filled out");
-        return false;
-    }else {
-        M.toast({html: 'Mensagem enviada com sucesso!'})
+    const name = document.forms["consulta"]["nome"].value
+    const tname = name.toLowerCase().trim().split(" ")
+    // // const email = null
+    const phone = document.forms["consulta"]["phone"].value
+    const mensage = document.forms["consulta"]["mensage"].value
+
+    if (tname.length <= 1) {
+        alert("Favor Preencher Campo Nome com Nome e Sobrenome")
+        return false
+    }else if (phone.length > 11 || phone.length < 11) {
+        alert("Seu Número Deve Conter 11 dígitos incluindo o DDD")
+        return false
+    }else if (mensage.length > 120) {
+        alert("Sua Mensagem Deve ter no Máximo 120 Caracteres")
+        return false
     }
-    // alert('msg')
+    else {
+        M.toast({html: 'Mensagem enviada com sucesso!'})
+        return true
+    }
+}
+
+addSpaces() {
+    // initial.replace("/([0-9]{3})/","\1 ");
+    // initial.replace("/[0-9]{3} ([0-9]{3})/","\1 ");
+    // return initial;
+    alert('msg')
 }
