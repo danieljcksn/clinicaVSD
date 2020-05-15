02f523faf7a070b773299d079dacb97ad6281c18
daniielcavallcante <daniiel.cavallcante@gmail.com>
2020-05-14 22:20:28 -0300

    Inicializador do Data-Picker

--- scr/js/index.js
+++ scr/js/index.js
@@ -9,3 +9,7 @@ function leftSlider() {
   const scroll = document.getElementById('cards-equipe')
   scroll.scrollLeft -= 50;
 }
+
+$(document).ready(function(){
+  $('.datepicker').datepicker();
+});
